ef33bd4ecdc265b14b4a52259396f6a8337fef1d
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@protonmail.com>
2021-07-08 10:02:02 -0300

    Update insert_token.php

--- workers/api/php/insert_token.php
+++ workers/api/php/insert_token.php
@@ -1,23 +1,24 @@
 <?php
-    ini_set('session.use_strict_mode', 1);
-    ini_set('session.cookie_httponly', 0);
-    ini_set('session.cookie_secure', 1);
-    ini_set('session.cookie_samesite', 'Strict');
+	 //VALUES FOR SECURE REQUESTS
+    //ini_set('session.use_strict_mode', 1);
+    //ini_set('session.cookie_httponly', 0);
+    //ini_set('session.cookie_secure', 1);
+    //ini_set('session.cookie_samesite', 'Strict');
     header('Content-Type: application/json; charset=utf-8');
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    //header('Access-Control-Allow-Origin: *');
+    //header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    //header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     require_once('sql.php');
     $arr = array();
-    $statement = 'INSERT INTO `validation` (`key`, `id`) VALUES ("'.$GET['key'].'","'.$GET['user_id'].'")';
-    $statement = mysqli_query($obj->conn,$statement);
-    while ($row = $statement->fetch_assoc()){
-		  $jsonData = '{"results":[';
-        $line = new stdClass;
-        $line->answer = 'OK';      
-        $arr[] = json_encode($line);
-        $jsonData .= implode(",", $arr);     
-        $jsonData .= ']}';
+    if($_SERVER['REQUEST_METHOD'] === 'GET') {
+    	$key = $_GET['key'];
+    	$uid = $_GET['user_id'];
     }
-    echo $jsonData;
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    	$key = $_POST['key'];
+    	$uid = $_POST['user_id'];    	
+    }
+    $statement = 'INSERT INTO `validation` (`key`, `id`) VALUES ("'.$key.'","'.$uid.'")';
+    $statement = mysqli_query($obj->conn,$statement);
+    echo json_encode('OK');
 ?>
